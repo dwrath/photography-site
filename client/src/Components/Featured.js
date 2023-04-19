23b1d4ec11d9bd16d006298265c27994652444d4
Damian Wrather <damian.wrather@gmail.com>
2023-04-19 16:19:17 -0500

    slider wrapping

--- client/src/Components/Featured.js
+++ client/src/Components/Featured.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import heroImg from '../images/heroImg.jpeg'
+
 const images = [
     { url: heroImg, alt: 'Image 1' },
     { url: heroImg, alt: 'Image 2' },
@@ -10,12 +11,14 @@ const images = [
 
 const Featured = () => {
     const [translateX, setTranslateX] = useState(0);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
             setTranslateX(prevTranslateX => {
                 if (prevTranslateX <= -2000) {
-                    return 0;
+                    setCurrentImageIndex((currentImageIndex + 1) % images.length);
+                    return prevTranslateX + 2000;
                 } else {
                     return prevTranslateX - 1;
                 }
@@ -23,24 +26,24 @@ const Featured = () => {
         }, 20);
 
         return () => clearInterval(intervalId);
-    }, []);
+    }, [currentImageIndex]);
 
     return (
-    <div className="container justify-center">
-        <div className="row">
-            <div className="column align-center ticker-container">
-                <div className="ticker-tape" style={{ transform: `translateX(${translateX}px)`}}>
-                    {images.map((image, index) => (
-                        <img
-                            key={index}
-                            src={image.url}
-                            alt={image.alt}
-                        />
-                    ))}
+        <div className="container justify-center">
+            <div className="row">
+                <div className="column align-center ticker-container">
+                    <div className="ticker-tape" style={{ transform: `translateX(${translateX}px)` }}>
+                        {[...images, ...images].map((image, index) => (
+                            <img
+                                key={index}
+                                src={image.url}
+                                alt={image.alt}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 };
 
