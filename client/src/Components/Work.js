b26a68356fd29c6dc21178729979b2a6d4465401
Damian Wrather <damian.wrather@gmail.com>
2023-04-21 13:43:57 -0500

    Works section

--- client/src/Components/Work.js
+++ client/src/Components/Work.js
@@ -2,13 +2,18 @@ function Work() {
     return(
         <div className="container">
             <div className="row">
-                <div className="column">
+                <div className="works-header">
+                    <h1>Works</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="column works gap-10">
                     <div className="gym"></div>
                     <div className="events"></div>
-
                 </div>
-                <div className="column">
-                    <div></div>
+                <div className="column gap-10">
+                    <div className="city"></div>
+                    <div className="animals"></div>
                 </div>
             </div>
         </div>
