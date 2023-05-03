3c5a8c5b5217323a5b5337f6ec04e8efcd393702
Damian Wrather <damian.wrather@gmail.com>
2023-05-03 17:00:11 -0500

    one works click here link put in

--- client/src/Components/Work.js
+++ client/src/Components/Work.js
@@ -1,7 +1,9 @@
 import { motion } from "framer-motion"
 import { useInView } from "react-intersection-observer"
-import gym from '../images/gym.jpg'
-import animals from '../images/animals.jpg'
+import gym from '../images/gym-cover.jpg'
+import animals from '../images/dog-cover.jpg'
+import events from '../images/events-cover.jpg';
+import city from '../images/city-cover.jpg';
 
 function Work() {
     const { ref, inView } = useInView({
@@ -18,20 +20,26 @@ function Work() {
             </div>
             <div className="row">
                 <div className="column works gap-10">
+                    <div className="works-overlay">
                     <motion.div
                         className="gym"
                         initial={{ opacity: 0 }}
                         animate={inView ? { opacity: 1 } : { opacity: 0 }}
                         transition={{ duration: 0.5 }}
                     >
-                        <img className="work-images" src={gym}/>
+                        <img className="work-images" src={gym} alt="gym"/>
+
+                            <a className="fade-in-link">click to see Gym photos</a>
                     </motion.div>
+                    </div>
                     <motion.div
                         className="events"
                         initial={{ opacity: 0 }}
                         animate={inView ? { opacity: 1 } : { opacity: 0 }}
                         transition={{ duration: 0.5, delay: 0.2 }}
-                    ></motion.div>
+                    >
+                        <img className="work-images" src={events} alt="wedding"/>
+                    </motion.div>
                 </div>
                 <div className="column gap-10">
                     <motion.div
@@ -40,14 +48,16 @@ function Work() {
                         animate={inView ? { opacity: 1 } : { opacity: 0 }}
                         transition={{ duration: 0.5, delay: 0.4 }}
                     >
-                        <img className="work-images" src={animals}/>
+                        <img className="work-images" src={animals} alt="dog"/>
                     </motion.div>
                     <motion.div
                         className="animals"
                         initial={{ opacity: 0 }}
                         animate={inView ? { opacity: 1 } : { opacity: 0 }}
                         transition={{ duration: 0.5, delay: 0.6 }}
-                    ></motion.div>
+                    >
+                        <img className="work-images" src={city} alt="city"/>
+                    </motion.div>
                 </div>
             </div>
         </div>
